25287ab6fb5d4d88e28fa5d92804acb9aa1219d9
Muhmmad Najam Us Saqib <bc210411582mnu@vu.edu.pk>
2025-05-03 07:58:52 +0500

    Update candidate_router.py

--- routers/candidate_router.py
+++ routers/candidate_router.py
@@ -1,9 +1,9 @@
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from ..database import SessionLocal
-from .. import schemas
-from ..services import candidate_service
+from database import SessionLocal
+from  import schemas
+from services import candidate_service
 
 router = APIRouter()
 
